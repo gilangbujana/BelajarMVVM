be7dcfa27a76f42355b6cccb80a87db809212d39
gilangbujana <noviangilang@gmail.com>
2019-12-30 16:03:59 +0700

    [ADD] add Preferences package in data package [ADD] add PreferenceProvider in Preference package

--- app/src/main/java/com/example/belajarmvvm/data/network/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/example/belajarmvvm/data/network/NetworkConnectionInterceptor.kt
@@ -2,6 +2,7 @@ package com.example.belajarmvvm.data.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import com.example.belajarmvvm.util.NoInternetException
 import okhttp3.Interceptor
 import okhttp3.Response
@@ -19,10 +20,23 @@ class NetworkConnectionInterceptor(
     }
 
     private fun isInternetAvailable() : Boolean{
-        val connectivityManager =
+       /* val connectivityManager =
             applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         connectivityManager.activeNetworkInfo.also {
             return it != null && it.isConnected
+        }*/
+        var result = false
+        val connectivityManager =
+            applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        connectivityManager?.let{
+            it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
+                result = when{
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            }
         }
+        return result
     }
 }
