44bc7493b50f6c3953eeed8415d57b93484b570e
gilangbujana <noviangilang@gmail.com>
2019-12-30 15:41:23 +0700

    [ADD] add Preferences package in data package [ADD] add PreferenceProvider in Preference package

--- app/src/main/java/com/example/belajarmvvm/ui/home/quotes/QuotesFragment.kt
+++ app/src/main/java/com/example/belajarmvvm/ui/home/quotes/QuotesFragment.kt
@@ -7,14 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.belajarmvvm.R
+import com.example.belajarmvvm.data.db.entities.Quote
 import com.example.belajarmvvm.util.Coroutines
-import com.example.belajarmvvm.util.toast
+import com.example.belajarmvvm.util.hide
+import com.example.belajarmvvm.util.show
+
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.quotes_fragment.*
 import org.kodein.di.android.x.kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
-import java.time.LocalDateTime
+
 
 class QuotesFragment : Fragment(), KodeinAware {
     override val kodein by kodein()
@@ -33,13 +40,40 @@ class QuotesFragment : Fragment(), KodeinAware {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this, factory).get(QuotesViewModel::class.java)
+        bindUI()
 
-        Coroutines.main {
+        /*(Coroutines.main {
             val quotes = viewModel.quotes.await()
             quotes.observe(this, Observer {
                 context?.toast(it.size.toString())
             })
+        }*/
+    }
+
+    private fun bindUI() = Coroutines.main {
+        pb_rv_quotes.show()
+        viewModel.quotes.await().observe(this, Observer {
+            pb_rv_quotes.hide()
+            initRecyclerView(it.toQuoteItem())
+        })
+    }
+
+    private fun initRecyclerView(quoteItem: List<QuoteItem>) {
+        val mAdapter = GroupAdapter<GroupieViewHolder>().apply {
+            addAll(quoteItem)
+        }
+        rv_quotes.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = mAdapter
         }
     }
+
+    private fun List<Quote>.toQuoteItem() : List<QuoteItem>{
+        return this.map {
+            QuoteItem(it)
+        }
+    }
+
 
 }
